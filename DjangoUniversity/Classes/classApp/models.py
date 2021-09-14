4c5eccf40e0d2a5e054bc5b3367c430614665ca2
[MtSoftDevSolutions] <87912940+MtSoftDevSolutions@users.noreply.github.com>
2021-09-14 13:54:28 -0400

    confused

--- DjangoUniversity/Classes/classApp/models.py
+++ DjangoUniversity/Classes/classApp/models.py
@@ -8,9 +8,9 @@ class DjangoClasses(models.Model):
     Duration = models.FloatField()
 
 
-# a = djangoClasses(Title='Defense Against the Dark Arts', Course_Number=101, Instructor_Name='Snape', Duration=2.5)
-# a.save()
-# b = djangoClasses(Title='Herbology', Course_Number=50, Instructor_Name='Sprout', Duration=2)
-# b.save()
-# c = djangoClasses(Title='Muggle Studies', Course_Number=10, Instructor_Name='Burbage', Duration=1)
-# c.save()
+a = DjangoClasses(Title='Defense Against the Dark Arts', Course_Number=101, Instructor_Name='Snape', Duration=2.5)
+a.save()
+b = DjangoClasses(Title='Herbology', Course_Number=50, Instructor_Name='Sprout', Duration=2)
+b.save()
+c = DjangoClasses(Title='Muggle Studies', Course_Number=10, Instructor_Name='Burbage', Duration=1)
+c.save()
